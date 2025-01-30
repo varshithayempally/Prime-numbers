7a2d835b336f0cc76d02104d505f94403a25db01
varshithayempally <varshithayempally@gmail.com>
2025-01-30 21:46:09 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,24 @@
+public class Prime {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false; 
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; 
+            }
+        }
+        return true; 
+    }
+
+    public static void main(String[] args) {
+        int number = 29;
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
+
+
